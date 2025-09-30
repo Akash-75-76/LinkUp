a34fb327612d3ed65326abe939959ef4f3036bfe
Akash-75-76 <deshmukhaakash752005@gmail.com>
2025-09-30 12:34:16 +0530

    created login router and controller with testing with apihttp

--- backend/controllers/user.controller.js
+++ backend/controllers/user.controller.js
@@ -1,6 +1,7 @@
 import User from "../models/user.model.js";
 import bcrypt from "bcrypt";
 import Profile from "../models/profile.model.js";
+import crypto from "crypto";
 export const register = async (req, res) => {
   try {
     const { name, username, email, password } = req.body;
@@ -27,3 +28,28 @@ export const register = async (req, res) => {
     return res.status(500).json({ message: error.message });
   }
 };
+
+
+export const login=async(req,res)=>{
+    try {
+        const { email, password } = req.body;
+        if (!email || !password) {
+            return res.status(400).json({ message: "All fields are required" });
+        }
+
+        const user = await User.findOne({ email });
+        if (!user) {
+            return res.status(404).json({ message: "User not found" });
+        }
+
+        const isMatch = await bcrypt.compare(password, user.password);
+        if (!isMatch) {
+            return res.status(401).json({ message: "Invalid credentials" });
+        }
+        const token=crypto.randomBytes(32).toString('hex');
+        await User.updateOne({ _id: user._id }, { $set: { token } });
+        return res.status(200).json({ message: "Login successful", token });
+    } catch (error) {
+        return res.status(500).json({ message: error.message });
+    }
+}
